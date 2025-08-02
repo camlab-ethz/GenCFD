2b3fadff5c2ed81b2623bed0b61d5478364001e0
yramic <yannick.ramic@outlook.com>
2025-08-02 11:26:34 +0200

    Linear Attention Implementation

--- GenCFD/model/building_blocks/layers/linear_attention.py
+++ GenCFD/model/building_blocks/layers/linear_attention.py
@@ -15,8 +15,19 @@
 import torch
 import torch.nn as nn
 from torch.nn import functional as F
-import einops
+from einops import rearrange, repeat
 from functools import partial
+
+
+class RMSNorm(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.scale = dim ** 0.5
+        self.g = nn.Parameter(torch.ones(1, dim, 1, 1))
+
+    def forward(self, x):
+        return F.normalize(x, dim = 1) * self.g * self.scale
+
 
 class LinearAttention(nn.Module):
     def __init__(
@@ -31,14 +42,14 @@ class LinearAttention(nn.Module):
         self.heads = heads
         hidden_dim = dim_head * heads
 
-        self.norm = nn.RMSNorm(dim)
+        self.norm = RMSNorm(dim)
 
         self.mem_kv = nn.Parameter(torch.randn(2, heads, dim_head, num_mem_kv))
         self.to_qkv = nn.Conv2d(dim, hidden_dim * 3, 1, bias = False)
 
         self.to_out = nn.Sequential(
             nn.Conv2d(hidden_dim, dim, 1),
-            nn.RMSNorm(dim)
+            RMSNorm(dim)
         )
 
     def forward(self, x):
@@ -47,9 +58,9 @@ class LinearAttention(nn.Module):
         x = self.norm(x)
 
         qkv = self.to_qkv(x).chunk(3, dim = 1)
-        q, k, v = map(lambda t: einops.rearrange(t, 'b (h c) x y -> b h c (x y)', h = self.heads), qkv)
+        q, k, v = map(lambda t: rearrange(t, 'b (h c) x y -> b h c (x y)', h = self.heads), qkv)
 
-        mk, mv = map(lambda t: einops.einops(t, 'h c n -> b h c n', b = b), self.mem_kv)
+        mk, mv = map(lambda t: repeat(t, 'h c n -> b h c n', b = b), self.mem_kv)
         k, v = map(partial(torch.cat, dim = -1), ((mk, k), (mv, v)))
 
         q = q.softmax(dim = -2)
@@ -60,5 +71,5 @@ class LinearAttention(nn.Module):
         context = torch.einsum('b h d n, b h e n -> b h d e', k, v)
 
         out = torch.einsum('b h d e, b h d n -> b h e n', context, q)
-        out = einops.rearrange(out, 'b h c (x y) -> b (h c) x y', h = self.heads, x = h, y = w)
+        out = rearrange(out, 'b h c (x y) -> b (h c) x y', h = self.heads, x = h, y = w)
         return self.to_out(out)
